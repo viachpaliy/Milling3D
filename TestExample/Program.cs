d34896f61877a8d4de91cd3a6c57789c288607ff
viachpaliy <viachpaliy@ukr.net>
2021-06-28 21:35:11 +0300

    Initial commit

---
+++ TestExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
